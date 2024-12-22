417ca51d8280162b4321c14197a5a221c32bad5c
utibori-jp <kouki.utibori@gmail.com>
2024-12-22 11:11:11 +0900

    feat: enable PostgreSQL connection

---
+++ backend/api/postgres.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func InitDB() error {
+	var err error
+
+	USER := "postgres"
+	PASSWORD := "postgres"
+	HOST := "db"
+	PORT := "5432"
+	DBNAME := "db"
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", USER, PASSWORD, HOST, PORT, DBNAME)
+
+	DB, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Println("Unable to connect to database: ", err)
+		return err
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Println("Unable to ping database: ", err)
+		return err
+	}
+
+	fmt.Println("\033[32mConnected to PostgreSQL successfully\033[0m")
+	return nil
+}
+
+func CloseDB() {
+	if DB != nil {
+		defer func() {
+			err := DB.Close()
+			if err != nil {
+				fmt.Printf("Error closing the database connection: %v\n", err)
+			} else {
+				fmt.Println("Database connection closed.")
+			}
+		}()
+	}
+}
--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"go-backend/api"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,13 @@ func main() {
 	server := &api.ProductsServer{
 		ServerId: 1, // Sample Value
 	}
+
+	err := api.InitDB()
+	if err != nil {
+		log.Fatal("\033[31mError connecting to the database:", err, "\033[0m")
+	}
+	defer api.CloseDB()
+
 	api.RegisterHandlers(r, server)
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
